21f4f354f9aa392c19c0db1965cb431efee87903
DrPeppr <md.purcaru@gmail.com>
2023-03-10 20:30:25 +0000

    fixed questions

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -20,13 +20,13 @@ const questions = [
         correctAnswer: "<script>"
     },
     // {
-    //     question: `What is the correct JavaScript syntax to change the content of the HTML element below <p id="demo">This is a demonstration.</p>?`,
-    //     answers: [
+    //     question: What is the correct JavaScript syntax to change the content of the HTML element below `<p id="demo">This is a demonstration.</p>?`,
+    //     answers: `
     //         `<document.getElementById("demo").innerHTML = "Hello World!";>`,
     //         `<demo.innerHTML = "Hello World!">`
     //         `<document.getElementByName("p").innerHTML = "Hello World!";>`
     //         `<document.getElement("p").innerHTML = "Hello World!";>`
-    //     ],
+    //     `,
     //     correctAnswer: `<document.getElementById("demo").innerHTML = "Hello World!";>`
     // },
     {
@@ -62,6 +62,8 @@ const submitBtn = document.getElementById('submit');
 
 let currentPosition = 0;
 let time = 60;
+let currentQuestion = {}
+let acceptingAnswers = true
 
 startBtn.addEventListener("click",startGame)
 
@@ -93,6 +95,9 @@ function setNextQuestion(){
     document.querySelectorAll(".btn")[2].innerText = questions[currentPosition].answers[2];
     document.querySelectorAll(".btn")[3].innerText = questions[currentPosition].answers[3];
    currentPosition++
+   if(currentPosition === 4) {
+    endGame();
+   }
 }
 
 choicesContainer.addEventListener("click",function(event){
@@ -102,6 +107,7 @@ choicesContainer.addEventListener("click",function(event){
     }
     else if(event.target.innerText !== questions[currentPosition].correctAnswer){
         time-10;
+        setNextQuestion();
     }
 })
 
@@ -109,11 +115,11 @@ choicesContainer.addEventListener("click",function(event){
 function endGame() {
     console.log("end-game")
     clearInterval(timerId)
-    question
+    
 }
-//you need to set an if statement for if the timer equals 0 to end the Game
+//need to set an if statement for if the timer equals 0 to end the Game
 
-//you need to set an event for when the game ends, to display the high scores div, append the high scores in a for loop to the table, and also sort the high scores array before doing so. you need to push the initials and time left into the high scores array and then re-setItem for localStorage, overwriting the previous array
+//need to set an event for when the game ends, to display the high scores div, append the high scores in a for loop to the table, and also sort the high scores array before doing so. you need to push the initials and time left into the high scores array and then re-setItem for localStorage, overwriting the previous array
 
 document.getElementById("submit").addEventListener("click",function(event){
     event.preventDefault();
